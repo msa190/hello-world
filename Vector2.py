fc14813a6014166e22c3efd23c0e9e63e1c3fc58
msa190 <mauricio98sousa@gmail.com>
2017-08-25 22:57:15 -0300

    Update Vector2.py

--- Vector2.py
+++ Vector2.py
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class Vector():
 	v = []
 	def __init__(self, w=[]):
@@ -15,17 +16,47 @@ class Vector():
 
 	def __setitem__(self, key, value):
 		self.v[key] = value
-	
+	#soma
 	def __add__(self, w):
 		if(w.__class__.__name__!= 'Vector'):
 			raise
-		u = self		
+		u = self
+		k=[0.0 for i in range(u)]
 		for i in range(len(u)):
-			u[i] = w[i]+ u[i]
-		return u
+			k[i] = w[i]+ u[i]
+		return k
+	#subtracao
+	def __sub__(self,w):
+		if(w.__class__.__name__!= 'Vector'):
+			raise
+		u = self
+		k =[0.0 for i in range(u)]
+		for i in range(len(u)):
+			k[i]= u[i]-w[i]
+		return k
+	#multiplicacao por escalar
+	def __mul__(self,alpha):
+		#if(alpha.__class__.__name__!='Vector'):
+		#raise
+		u = self
+		k = [0.0 for i in u]
+		#map(lambda x: alpha*x, self.v)
+		for i in range(len(u)):
+			k[i]= u[i]*alpha
+		return k
+	#comutatividade da multiplicacao por escalar
+	#def __rmul__(self, u):
+	#	return u*self
+	def escalar(self,w=[]):
+		soma = 0.0
+		u = self
+		#if(len(u)!=len(w)):
+		#	raise
+		for i in range(len(u)):
+			u[i] = u[i]*w[i]
+		for i in range(len(u)):
+			soma = soma + u[i]
+		return soma
+		
 	def __repr__(self):
-		return repr(self.v)
-			 
-a = Vector([5,3,4])
-b = Vector([1,2,4])
-print(a+b)
+		return repr(self.v)	
